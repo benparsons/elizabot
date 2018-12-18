3e38d7eef9b331e88493ae2c67f76e0060b98749
Ben Parsons <ben@bpulse.co.uk>
2018-12-18 15:48:25 +0000

    super-simple version of the implementation

---
+++ simple.js
@@ -0,0 +1,49 @@
+const Eliza = require('eliza-as-promised');
+const MatrixClient = require("matrix-bot-sdk").MatrixClient;
+const AutojoinRoomsMixin = require("matrix-bot-sdk").AutojoinRoomsMixin;
+
+var config = require('config');
+var access_token = "";
+if (config.has('access_token')) {
+    access_token = config.get('access_token');
+} else {
+    console.log("mising access_token");
+    process.exit(1);
+}
+
+const client = new MatrixClient("https://matrix.org", access_token);
+AutojoinRoomsMixin.setupOnClient(client);
+client.start().then(() => console.log("Client started!"));
+
+var elizas = {};
+client.on("room.join", (roomId) => {
+    elizas[roomId] = {
+        eliza: new Eliza(),
+        last: (new Date()).getTime()
+    }
+    client.sendMessage(roomId, {
+        "msgtype": "m.notice",
+        "body": elizas[roomId].eliza.getInitial()
+    });
+});
+client.on("room.message", (roomId, event) => {
+    elizas[roomId].eliza.getResponse(event.content.body)
+        .then((response) => {
+            var responseText = '';
+            if (response.reply) { responseText = response.reply; }
+            if (response.final) { responseText = response.final; }
+
+            client.sendMessage(roomId, {
+                "msgtype": "m.notice",
+                "body": responseText,
+                "responds": {
+                    "sender": event.sender,
+                    "message": event.content.body
+                }
+            }).then((eventId) => {
+                if (response.final) {
+                    client.leaveRoom(roomId);
+                }
+            });
+    });
+});
